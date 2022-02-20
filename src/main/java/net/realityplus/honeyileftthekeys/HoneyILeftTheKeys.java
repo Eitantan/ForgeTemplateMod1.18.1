b90abd85d35d6c00284ee9282c65880427f75203
AlperstudiosLD <eitan.alperstein@gmail.com>
2022-02-19 20:38:10 -0700

    Mod citrine into game alpha

--- src/main/java/net/realityplus/honeyileftthekeys/HoneyILeftTheKeys.java
+++ src/main/java/net/realityplus/honeyileftthekeys/HoneyILeftTheKeys.java
@@ -1,35 +1,31 @@
-package net.realityplus.tutorialmod;
+package net.realityplus.honeyileftthekeys;
 
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.realityplus.honeyileftthekeys.item.ModItems;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TutorialMod.MOD_ID)
-public class TutorialMod
+@Mod(HoneyILeftTheKeys.MOD_ID)
+public class HoneyILeftTheKeys
 {
-    public static final String MOD_ID = "tutorialmod";
+    public static final String MOD_ID = "honeyileftthekeys";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     // TEST COMMIT FOR GITHUB
 
-    public TutorialMod() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    public HoneyILeftTheKeys() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -40,5 +36,6 @@ public class TutorialMod
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        LOGGER.info("AIR BLOCK >> {}", Blocks.AIR.getRegistryName());
     }
 }
